f3c37dfdb0def7d2d8956e5a02c8045fd5e9992b
kush1410-coder <kushvarshney17@gmail.com>
2025-07-08 21:22:09 +0530

    Add files via upload

---
+++ p.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cin>>n;
+    int arr[];
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    }
+    int max1=arr[0],max2=arr[1];
+    for(int i=1;i<n;i++){
+        if()
+    }
+}
